01932759d5189c48237139c1b882bfcde0b7a1fd
Evgen Lyulyuk <zenyalyulyuk@gmail.com>
2024-12-30 18:49:23 +0200

    JRTB-2 added telegram bot to the project

--- src/main/java/com/githab/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplication.java
+++ src/main/java/com/githab/javarushcommunity/javarush_telegrambot/JavarushTelegrambotApplication.java
@@ -1,13 +1,27 @@
 package com.githab.javarushcommunity.javarush_telegrambot;
 
+import com.githab.javarushcommunity.javarush_telegrambot.bot.JavaRushTelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.telegram.telegrambots.meta.ApiConstants;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
-public class JavarushTelegrambotApplication {
+public abstract class JavarushTelegrambotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavarushTelegrambotApplication.class, args);
+	public static void main(String[] args) throws TelegramApiException {
+		TelegramBotsApi telegramBotsApi=new TelegramBotsApi(DefaultBotSession.class);
+		try{
+			telegramBotsApi.registerBot(new JavaRushTelegramBot());
+		}catch (TelegramApiRequestException e){
+
+		}
+		//SpringApplication.run(JavarushTelegrambotApplication.class, args);
 	}
 
 }
